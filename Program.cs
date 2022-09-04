78859f17dbe5dd89c996b9c5addc8d99b2b483d5
Natali <nat.kolodeznickova@yandex.ru>
2022-09-04 14:27:47 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+//Console.Clear();
+//Console.WriteLine($"Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.");
+//int n = InputNumbers("Введите n: ");
+//int count = 2;
+//PrintNumber(n, count);
+//Console.Write(1);
+
+//void PrintNumber(int n, int count)
+//{
+  //if (count > n) return;
+  //PrintNumber(n, count + 1);
+  //Console.Write(count + ", ");
+//}
+
+//int InputNumbers(string input) 
+//{
+ // Console.Write(input);
+ // int output = Convert.ToInt32(Console.ReadLine());
+ // return output;
+//}
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.WriteLine($"Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
